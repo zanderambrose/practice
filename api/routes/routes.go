b6ff87608b2e4a98c20f75450604c90a0b9a5089
Zander Ambrose <alexander.d.ambrose@gmail.com>
2024-05-16 21:46:25 -0400

    POC for token auth.

--- api/routes/routes.go
+++ api/routes/routes.go
@@ -1,12 +1,38 @@
 package api
 
 import (
+	"crypto/sha256"
+	"crypto/subtle"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/keyauth"
 	"whoshittin/api/handlers"
 )
 
+func validateAPIKey(c *fiber.Ctx, token string) (bool, error) {
+	// Mock db
+	apiKeys := map[string]string{
+		"client_id1": "api_key",
+		"client_id2": "api_key2",
+	}
+
+	clientId := c.Get("X-Client-ID")
+
+	dbKey := apiKeys[clientId]
+	hashedAPIKey := sha256.Sum256([]byte(dbKey))
+	hashedKey := sha256.Sum256([]byte(token))
+	if subtle.ConstantTimeCompare(hashedAPIKey[:], hashedKey[:]) == 1 {
+		return true, nil
+	}
+
+	return false, keyauth.ErrMissingOrMalformedAPIKey
+}
+
 func ApplyRoutes(app *fiber.App) {
 	app.Get("/api/v1/collections", handlers.ListCollections)
+
+	app.Use(keyauth.New(keyauth.Config{
+		Validator: validateAPIKey,
+	}))
 
 	app.Get("/api/v1/:venue", handlers.GetVenueLineup)
 	// TODO - This needs auth
