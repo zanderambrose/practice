bd97c7e572723d88a71423e2da8fa627fb977dbd
Zander Ambrose <alexander.d.ambrose@gmail.com>
2024-05-17 08:34:20 -0400

    WIP, auth db integration.

--- api/routes/routes.go
+++ api/routes/routes.go
@@ -3,21 +3,46 @@ package api
 import (
 	"crypto/sha256"
 	"crypto/subtle"
+	"errors"
+	"fmt"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/keyauth"
+	"go.mongodb.org/mongo-driver/bson"
 	"whoshittin/api/handlers"
+	"whoshittin/api/utils"
 )
 
-func validateAPIKey(c *fiber.Ctx, token string) (bool, error) {
-	// Mock db
-	apiKeys := map[string]string{
-		"client_id1": "api_key",
-		"client_id2": "api_key2",
+type Client struct {
+	ClientId    string       `json:"clientId" bson:"clientId"`
+	ApiKey      string       `json:"apiKey" bson:"apiKey"`
+	Permissions []Permission `json:"permissions" bson:"permissions"`
+}
+
+type Permission struct {
+	Entry   string `json:"entry" bson:"entry"`
+	IsAdmin string `json:"isAdmin" bson:"isAdmin"`
+}
+
+func queryClient(clientId string) (Client, error) {
+	var client Client
+	err := db.GetCollection("client").FindOne(db.CTX, bson.M{"clientId": clientId}).Decode(&client)
+	if err != nil {
+		fmt.Println("Error finding client")
+		return Client{}, errors.New("Error finding client")
 	}
 
+	return client, nil
+}
+
+func validateAPIKey(c *fiber.Ctx, token string) (bool, error) {
 	clientId := c.Get("X-Client-ID")
 
-	dbKey := apiKeys[clientId]
+	client, err := queryClient(clientId)
+	if err != nil {
+		return false, errors.New("Not a valid client id")
+	}
+
+	dbKey := client.ApiKey
 	hashedAPIKey := sha256.Sum256([]byte(dbKey))
 	hashedKey := sha256.Sum256([]byte(token))
 	if subtle.ConstantTimeCompare(hashedAPIKey[:], hashedKey[:]) == 1 {
@@ -27,21 +52,20 @@ func validateAPIKey(c *fiber.Ctx, token string) (bool, error) {
 	return false, keyauth.ErrMissingOrMalformedAPIKey
 }
 
+func validateRequest(c *fiber.Ctx, token string) (bool, error) {
+	return true, nil
+}
+
 func ApplyRoutes(app *fiber.App) {
 	app.Get("/api/v1/collections", handlers.ListCollections)
 
 	app.Use(keyauth.New(keyauth.Config{
-		Validator: validateAPIKey,
+		Validator: validateRequest,
 	}))
 
 	app.Get("/api/v1/:venue", handlers.GetVenueLineup)
-	// TODO - This needs auth
 	app.Post("/api/v1/:venue", handlers.UpdateLineupV1)
-	// TODO - This needs auth
 	app.Delete("/api/v1/:venue", handlers.DeleteCollection)
-
-	// TODO - This needs auth
 	app.Post("/api/v1/scrape/venues", handlers.ScrapeVenues)
-	// TODO - This needs auth
 	app.Post("/api/v1/scrape/:venue", handlers.ScrapeVenue)
 }
