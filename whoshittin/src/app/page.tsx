95bba6b0e2cc446ad1389ba21c5d9bd493408daa
Zander Ambrose <alexander.d.ambrose@gmail.com>
2024-05-18 13:25:12 -0400

    More cleanup.

--- whoshittin/src/app/page.tsx
+++ whoshittin/src/app/page.tsx
@@ -1,14 +1,12 @@
 import { Footer } from "@/components";
 import Hero from "./hero";
 import Posts from "./posts";
-import Articles from "./articles";
 
 export default function Campaign() {
     return (
         <>
             <Hero />
             <Posts />
-            <Articles />
             <Footer />
         </>
     );
--- whoshittin/src/components/index.ts
+++ whoshittin/src/components/index.ts
@@ -3,6 +3,4 @@
 export * from "./footer";
 export * from "./layout";
 export * from "./footer";
-export * from "./article-card";
 export * from "./blog-post-card";
-export * from "./fixed-plugin";
