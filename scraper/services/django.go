dd7a453d252fabf70f55a353a7c0c2be955a7f36
Zander Ambrose <alexander.d.ambrose@gmail.com>
2024-06-16 20:19:23 -0400

    Dynamically add django times.

--- scraper/services/django.go
+++ scraper/services/django.go
@@ -2,6 +2,7 @@ package scraper
 
 import (
 	"github.com/gocolly/colly"
+	"regexp"
 	"strings"
 	"sync"
 	"whoshittin/scraper/utils"
@@ -18,9 +19,25 @@ func Django(c *colly.Collector, w *sync.WaitGroup) {
 		eventData.AppendEventTitle(e.ChildText("h3"))
 		eventData.AppendEventLink(e.ChildAttr("a.details-container", "href"))
 		eventData.AppendEventImage(e.ChildAttr("img", "src"))
-		times := e.ChildText("p.event_card__time-pair")
-		eventTimes := strings.Replace(times, "|", "&", 1)
-		eventData.AppendEventTime(eventTimes)
+		times := e.ChildText("p.event_card__time-pair > span")
+		strippedTimes := strings.ReplaceAll(times, "|", "")
+		pattern := `(?i)(\d{1,2}:\d{2}[ap]m)`
+		re, err := regexp.Compile(pattern)
+		if err != nil {
+			println("Error compiling regexp: ", err)
+		}
+		splitTimes := re.FindAllString(strippedTimes, -1)
+
+		var allTimes string
+
+		for i, v := range splitTimes {
+			if i < len(splitTimes)-1 {
+				allTimes += v + "&"
+			} else {
+				allTimes += v
+			}
+		}
+		eventData.AppendEventTime(allTimes)
 		utils.PostVenueData(venueNames.Django, &eventData)
 	})
 	c.Visit("https://www.thedjangonyc.com/events")
